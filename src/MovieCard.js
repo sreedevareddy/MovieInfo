7514709744e998518fa2d377fb5d2f763c897d9d
Sreedeva <79521521+sreedevareddy@users.noreply.github.com>
2023-04-07 00:52:47 +0530

    first commit

---
+++ src/MovieCard.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=8026c305";
+
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  const [movieData, setMovieData] = useState("");
+  const [rating, setRating] = useState("");
+
+  useEffect(() => {
+    searchMovies(imdbID);
+  }, []);
+
+  const searchMovies = async (id) => {
+    const response = await fetch(`${API_URL}&i=${id}`);
+    const data = await response.json();
+
+    setMovieData(data);
+    setRating(movieData.Ratings[0]['Value']);
+  };
+
+  const logdata = () => {
+    console.log(movieData);
+  }
+
+
+  return (
+    <div onClick={logdata} className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"}
+          alt={Title}
+        />
+      </div>
+
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+        <h3>{rating}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
